c2a58ecaeed2b9e161051e1ae523b88daad8b071
Saravjot Singh Bharaj <161899117+Yoriichi47@users.noreply.github.com>
2025-01-06 17:39:18 -0500

    Implementation of filtering through City, State and country

--- src/app/api/rooms/route.ts
+++ src/app/api/rooms/route.ts
@@ -1,17 +1,28 @@
-import { allRooms, newRoom } from '@/app/controllers/roomControllers'
+import { allRooms, getRoomByLocation } from '@/app/controllers/roomControllers'
 import { createEdgeRouter } from 'next-connect'
 import { NextRequest } from 'next/server'
 
 interface RequestContext{
-    params: {
-        id: string
-    }
+    params: Promise<{city?: string; state?: string; country?: string}>
 }
 
 const router = createEdgeRouter<NextRequest, RequestContext>()
 
 router.get(allRooms)
+// router.get(getRoomByLocation)
 
 export async function GET(request:NextRequest, ctx: RequestContext) {
+    const searchParams = request.nextUrl.searchParams
+    const city = searchParams.get("city")
+    const state = searchParams.get("state")
+    const country = searchParams.get("country")
+    console.log({city, state, country})
+    
+    ctx.params = Promise.resolve({
+        city: city ?? undefined,
+        state: state ?? undefined,
+        country: country ?? undefined
+    })
+
     return router.run(request, ctx)
 }
--- src/app/controllers/roomControllers.ts
+++ src/app/controllers/roomControllers.ts
@@ -1,10 +1,54 @@
 import { prisma } from "@/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export const allRooms = async (req: NextRequest) => {
+export const allRooms = async (
+  req: NextRequest,
+  {
+    params,
+  }: { params: Promise<{ city?: string; state?: string; country?: string }> }
+) => {
   const rooms = await prisma.room.findMany();
   const responsePerPage = 6;
-  return NextResponse.json({ success: true, responsePerPage, rooms });
+  const city = (await params).city;
+  const state = (await params).state;
+  const country = (await params).country;
+
+  console.log(`Filtered property: ${city || state || country}`);
+
+  if (city) {
+    const rooms = await prisma.room.findMany({
+      where: {
+        City: {
+          equals: city,
+          mode: "insensitive",
+        }
+        
+      },
+    });
+    return NextResponse.json({ success: true, responsePerPage, rooms });
+  } else if (state) {
+    const rooms = await prisma.room.findMany({
+      where: {
+        State: {
+          equals: state,
+          mode: "insensitive",
+        }
+      },
+    });
+    return NextResponse.json({ success: true, responsePerPage, rooms });
+  } else if (country) {
+    const rooms = await prisma.room.findMany({
+      where: {
+        Country: {
+          equals: country,
+          mode: "insensitive",
+        }
+      },
+    });
+    return NextResponse.json({ success: true, responsePerPage, rooms });
+  } else {
+    return NextResponse.json({ success: true, responsePerPage, rooms });
+  }
 };
 
 export const newRoom = async (req: NextRequest) => {
@@ -49,14 +93,20 @@ export const getRoomDetails = async (
   }
 };
 
+export const getRoomByLocation = async () => {
+  // const searchParams = useSearchParams()
+  // const city = searchParams.get("city")
+  // return NextResponse.json({city})
+};
+
 export const updateRoom = async (
   req: NextRequest,
-  { params }: { params: Promise<{roomId: string}> }
+  { params }: { params: Promise<{ roomId: string }> }
 ) => {
   // Based on the room ID, update the room details
   try {
     const body = await req.json();
-    const roomId = (await params).roomId
+    const roomId = (await params).roomId;
     const room = await prisma.room.update({
       where: {
         roomId,
@@ -78,22 +128,24 @@ export const updateRoom = async (
   }
 };
 
-export const deleteRoom = async (req: NextRequest, {params}: {params: Promise<{roomId: string}>}) => {
-  const roomId = (await params).roomId
+export const deleteRoom = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ roomId: string }> }
+) => {
+  const roomId = (await params).roomId;
   try {
     const room = await prisma.room.delete({
       where: {
         roomId,
-      }
-    })
+      },
+    });
 
-    if(!room || !room.roomId){
-      return NextResponse.json({message: "Room not found"}, {status: 404})
+    if (!room || !room.roomId) {
+      return NextResponse.json({ message: "Room not found" }, { status: 404 });
     }
 
-    return NextResponse.json({success: true, message: "Room deleted"})
+    return NextResponse.json({ success: true, message: "Room deleted" });
   } catch (error) {
-    if(error instanceof Error)
-      console.log("error: ", error.stack)
+    if (error instanceof Error) console.log("error: ", error.stack);
   }
-}
+};
