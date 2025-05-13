ca7ca47104d7e23ed28fd84c75565795c148e791
Saravjot Singh Bharaj <jotbharaj35@gmail.com>
2025-05-13 12:14:21 -0400

    Fixing the room page design

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com","plus.unsplash.com", "res.cloudinary.com"],
+    remotePatterns: [{
+      protocol: "http",
+      hostname: "**.com",  // ** means that any thing canbe here
+      port: "",
+      pathname: "/**"
+    }
+    // "images.unsplash.com","plus.unsplash.com", "res.cloudinary.com"
+  ],
   },
 };
 
